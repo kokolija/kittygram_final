577cc7ef0e671f1b53e838865cd6e52d718767f0
Georgij Kokolija <kokolija@yandex.ru>
2026-02-02 13:53:20 +0000

    Add Actions

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,11 +1,9 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR.parent / '.env')
+env_path = BASE_DIR.parent / '.env'
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
